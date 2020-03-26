9ba4ed8259b0a8c8a9b35ea4858c0267d92cf508
ozguryesilbas@live.com <ozgur1586>
2020-03-26 11:29:58 +0300

    login ve home sayfaları metod ile dönüldü

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 var http = require("http");
-var fs = require("fs")
+var fs = require("fs");
+
+var homeController = function(req, res) {
+    //nodejs blocksuzdur, o yüzden işlem bitince function (err, data) çağarıyoruz
+    //bu fonksiyonlar (aynı şekilde function (req, res) de) callback fonksiyonlarıdır
+    //işlemlerin sırayla çalışmasını sağlamak için kullanılırlar
+    fs.readFile("index.html", function (err, data) {
+        res.write(data);
+        res.end("Html dosyası okundı");
+    });
+}
+
+var loginController = function(req, res) {
+    fs.readFile("login.html", function (err, data) {
+        res.write(data);
+        res.end("Html dosyası okundı");
+    });
+}
 
 var server = http.createServer(function (req, res) {
 
     if(req.url=="/"){
-        //nodejs blocksuzdur, o yüzden işlem bitince function (err, data) çağarıyoruz
-        //bu fonksiyonlar (aynı şekilde function (req, res) de) callback fonksiyonlarıdır
-        //işlemlerin sırayla çalışmasını sağlamak için kullanılırlar
-        fs.readFile("index.html", function (err, data) {
-            res.write(data);
-            res.end("Html dosyası okundı");
-        });
+        homeController(req,res);
     }
 
     if(req.url=="/login"){
-        fs.readFile("login.html", function (err, data) {
-            res.write(data);
-            res.end("Html dosyası okundı");
-        });
+        loginController(req,res);
     }
 
-
     console.log(req.url);
-
 
 });
 
