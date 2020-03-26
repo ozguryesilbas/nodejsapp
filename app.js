ee78b083bcf805caa3917ee0a64ba6c5979ee53d
ozguryesilbas@live.com <ozgur1586>
2020-03-26 12:03:03 +0300

    yönlendirme işlemleri expressee devredildi

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-var http = require('http');
+//var http = require('http');
 var fs = require('fs');
 var express = require('express'); //express ile aşağıdaki işlemleri daha kolay yapacağız
 
+var app = express(); //express nesnemizi oluşturduk
+
+app.get('/', function(req,res){
+    //nodejs blocksuzdur, o yüzden işlem bitince function (err, data) çağarıyoruz
+    //bu fonksiyonlar (aynı şekilde function (req, res) de) callback fonksiyonlarıdır
+    //işlemlerin sırayla çalışmasını sağlamak için kullanılırlar
+    fs.readFile("index.html", function (err, data) {
+        res.write(data);
+        res.end("Html dosyası okundu");
+    });
+});
+
+app.get('/login', function(req,res){
+    //nodejs blocksuzdur, o yüzden işlem bitince function (err, data) çağarıyoruz
+    //bu fonksiyonlar (aynı şekilde function (req, res) de) callback fonksiyonlarıdır
+    //işlemlerin sırayla çalışmasını sağlamak için kullanılırlar
+    fs.readFile("login.html", function (err, data) {
+        res.write(data);
+        res.end("Html dosyası okundu");
+    });
+});
+
+app.listen(8005);
+
+/*
 var homeController = function(req, res) {
     //nodejs blocksuzdur, o yüzden işlem bitince function (err, data) çağarıyoruz
     //bu fonksiyonlar (aynı şekilde function (req, res) de) callback fonksiyonlarıdır
@@ -35,4 +60,4 @@ var server = http.createServer(function (req, res) {
 
 });
 
-server.listen(8005);
+server.listen(8005); */
